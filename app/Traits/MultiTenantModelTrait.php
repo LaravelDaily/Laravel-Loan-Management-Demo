627d08f1446a705a3daef43051bb70730a5bb86e
Rm Yakovenko <rm.yakovenko@gmail.com>
2020-07-25 14:46:37 +0300

    Early returns in MultiTenantModelTrait

--- app/Traits/MultiTenantModelTrait.php
+++ app/Traits/MultiTenantModelTrait.php
@@ -9,23 +9,30 @@ trait MultiTenantModelTrait
 {
     public static function bootMultiTenantModelTrait()
     {
-        if (!app()->runningInConsole() && auth()->check()) {
-            $user = auth()->user();
-            static::creating(function ($model) {
-                $model->created_by_id = auth()->id();
-            });
-            if (!$user->is_admin) {
-                static::addGlobalScope('created_by_id', function (Builder $builder) use ($user) {
-                    $column = 'created_by_id';
-                    if ($user->is_analyst) {
-                        $column = 'analyst_id';
-                    } else if ($user->is_cfo) {
-                        $column = 'cfo_id';
-                    }
-                    $field = sprintf('%s.%s', $builder->getQuery()->from, $column);
-                    $builder->where($field, auth()->id());
-                });
-            }
+        if (app()->runningInConsole()) {
+            return;
         }
+
+        if (!auth()->check()) {
+            return;
+        }
+
+        $user = auth()->user();
+        static::creating(function ($model) {
+            $model->created_by_id = auth()->id();
+        });
+        if ($user->is_admin) {
+            return;
+        }
+        static::addGlobalScope('created_by_id', function (Builder $builder) use ($user) {
+            $column = 'created_by_id';
+            if ($user->is_analyst) {
+                $column = 'analyst_id';
+            } else if ($user->is_cfo) {
+                $column = 'cfo_id';
+            }
+            $field = sprintf('%s.%s', $builder->getQuery()->from, $column);
+            $builder->where($field, auth()->id());
+        });
     }
 }
